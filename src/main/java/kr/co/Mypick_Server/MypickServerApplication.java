71d36384c0e033b66862344d7d1a246ec7c26a53
ASH <soxyxs4996@gmail.com>
2024-12-11 15:44:49 +0900

    The First

---
+++ src/main/java/kr/co/Mypick_Server/MypickServerApplication.java
@@ -0,0 +1,13 @@
+package kr.co.Mypick_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypickServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypickServerApplication.class, args);
+	}
+
+}
